d2edb85f58a15a9aa602b73214d591f475d71057
sahazada11 <sahazada2001@gmail.com>
2026-01-16 10:11:27 +0530

    Secound Commit

---
+++ src/test/java/Demo/Practice2.java
@@ -0,0 +1,11 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Practice2 {
+	@Test
+	public void m2() {
+		System.out.println("After Change The Code");
+		System.out.println("Hello");
+	}
+}
